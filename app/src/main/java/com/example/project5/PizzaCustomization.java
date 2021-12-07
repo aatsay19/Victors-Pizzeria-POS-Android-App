2872b96745fe422442bd741fa82439019f518d70
pranavtailor <68711844+pranavtailor@users.noreply.github.com>
2021-12-06 22:59:00 -0500

    Added comments to pizza customization class

--- app/src/main/java/com/example/project5/PizzaCustomization.java
+++ app/src/main/java/com/example/project5/PizzaCustomization.java
@@ -7,10 +7,18 @@ import android.os.Bundle;
 
 //import static com.example.project5.MainMenu.IMAGE_RESOURCE;
 
+/**
+ * This is the java class that implements functionality to activity_pizza_customization.xml file
+ * @author Aatif Sayed, Pranav Tailor
+ */
 public class PizzaCustomization extends AppCompatActivity {
     public static ImageView imageOfPizza;
     public static TextView typeOfPizza;
 
+    /**
+     * Method that creates activity
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
