32f62f6335eefa433adc2fe71f0560588755f07e
pranavtailor <68711844+pranavtailor@users.noreply.github.com>
2021-12-06 22:37:41 -0500

    Added comments to CurrentOrder class

--- app/src/main/java/com/example/project5/CurrentOrder.java
+++ app/src/main/java/com/example/project5/CurrentOrder.java
@@ -3,8 +3,17 @@ package com.example.project5;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * This is the java class that implements functionality to activity_current_orders.xml file
+ * @author Aatif Sayed, Pranav Tailor
+ */
+
 public class CurrentOrder extends AppCompatActivity {
 
+    /**
+     * Method that creates activity
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
