aef93aa39ab1b08dc3c076f989c373ce21c2f4a4
pranavtailor <68711844+pranavtailor@users.noreply.github.com>
2021-12-06 22:40:57 -0500

    Added comments to MainMenu class

--- app/src/main/java/com/example/project5/MainMenu.java
+++ app/src/main/java/com/example/project5/MainMenu.java
@@ -6,6 +6,11 @@ import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * This is the java class that implements functionality to activity_main_menu.xml file
+ * @author Aatif Sayed, Pranav Tailor
+ */
+
 public class MainMenu extends AppCompatActivity {
 
     private Button orderDeluxeButton;
@@ -16,6 +21,10 @@ public class MainMenu extends AppCompatActivity {
     public static final String IMAGE_RESOURCE = "image_resource";
     public static String pizzaType = "Pizza Type: ";
 
+    /**
+     * Method that creates activity
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,6 +39,9 @@ public class MainMenu extends AppCompatActivity {
         handleStoreOrdersButtonClick();
     }
 
+    /**
+     * Method that changes activity to activity_pizza_customization with the deluxe button
+     */
     public void handleOrderDeluxeButtonClick() {
         orderDeluxeButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,6 +54,9 @@ public class MainMenu extends AppCompatActivity {
         });
     }
 
+    /**
+     * Method that changes activity to activity_pizza_customization with the hawaiian button
+     */
     public void handleOrderHawaiianButtonClick() {
         orderHawaiianButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -54,6 +69,9 @@ public class MainMenu extends AppCompatActivity {
         });
     }
 
+    /**
+     * Method that changes activity to activity_pizza_customization with the pepperoni button
+     */
     public void handleOrderPepperoniButtonClick() {
         orderPepperoniButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -66,6 +84,9 @@ public class MainMenu extends AppCompatActivity {
         });
     }
 
+    /**
+     * Method that changes activity to activity_current_orders when the 'view current order' button is pressed
+     */
     public void handleCurrentOrderButtonClick() {
         viewCurrentOrderButton = (Button) findViewById(R.id.viewCurrentOrderButton);
         viewCurrentOrderButton.setOnClickListener(new View.OnClickListener() {
@@ -76,6 +97,10 @@ public class MainMenu extends AppCompatActivity {
         });
     }
 
+    /**
+     * Method that changes activity to activity_list_of_store_orders when the 'view list of store orders' button
+     * is pressed
+     */
     public void handleStoreOrdersButtonClick() {
         viewListOfStoreOrdersButton = (Button) findViewById(R.id.viewListOfStoreOrdersButton);
         viewListOfStoreOrdersButton.setOnClickListener(new View.OnClickListener() {
@@ -86,11 +111,19 @@ public class MainMenu extends AppCompatActivity {
         });
     }
 
+    /**
+     * starts activity_current_orders activity
+     * used in methods above
+     */
     public void openCurrentOrdersActivity() {
         Intent intent = new Intent(this, CurrentOrder.class);
         startActivity(intent);
     }
 
+    /**
+     * starts activity_list_of_store_orders activity
+     * used in methods above
+     */
     public void openListOfStoreOrdersActivity() {
         Intent intent = new Intent(this, ListOfStoreOrders.class);
         startActivity(intent);
