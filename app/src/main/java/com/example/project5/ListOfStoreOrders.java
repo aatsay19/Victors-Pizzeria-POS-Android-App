e13d3503cb5b192fab7cd6e6718f66e33cd33960
pranavtailor <68711844+pranavtailor@users.noreply.github.com>
2021-12-06 22:38:30 -0500

    Added comments to listOfStoreOrders class

--- app/src/main/java/com/example/project5/ListOfStoreOrders.java
+++ app/src/main/java/com/example/project5/ListOfStoreOrders.java
@@ -3,8 +3,16 @@ package com.example.project5;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * This is the java class that implements functionality to activity_list_of_store_orders.xml file
+ * @author Aatif Sayed, Pranav Tailor
+ */
 public class ListOfStoreOrders extends AppCompatActivity {
 
+    /**
+     * Method that creates activity
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
